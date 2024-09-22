c2728955e65d53a716e415914c7d8df204e4a210
Ikbal Toriq <ikbal.toriq@bluebirdgroup.com>
2024-09-22 22:54:03 +0700

    - Implement pagination when scroll reaches the end

--- app/src/main/java/com/tor/movies/ui/screen/movies/Movies.kt
+++ app/src/main/java/com/tor/movies/ui/screen/movies/Movies.kt
@@ -1,19 +1,30 @@
 package com.tor.movies.ui.screen.movies
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.snapshotFlow
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.tor.movies.ui.theme.Dimens
+import kotlinx.coroutines.launch
 
 /**
 Created by ikbaltoriq on 20,September,2024
@@ -22,16 +33,21 @@ Created by ikbaltoriq on 20,September,2024
 @Preview(showSystemUi = true, device = Devices.PIXEL)
 @Composable
 fun Movies(moviesViewModel: MoviesViewModel = hiltViewModel()) {
+    val lazyListState = rememberLazyListState()
+
     val movies = moviesViewModel.movies.collectAsState(initial = emptyList())
-    val isLoading = moviesViewModel.isLoading.collectAsState(initial = true)
+    val isShimmerShow = moviesViewModel.isShimmerShow.collectAsState(initial = true)
     val isMoviesEmpty = moviesViewModel.isMoviesEmpty.collectAsState(initial = true)
+
     Column(modifier = Modifier.padding(Dimens.Medium)) {
+        Spacer(modifier = Modifier.height(60.dp))
         Search()
         when {
-            isLoading.value -> ShimmerMovies()
+            isShimmerShow.value -> ShimmerMovies()
             isMoviesEmpty.value -> EmptyMovies()
             else -> {
                 LazyColumn(
+                    state = lazyListState,
                     modifier = Modifier.fillMaxSize(),
                     verticalArrangement = Arrangement.spacedBy(Dimens.Small),
                     contentPadding = PaddingValues(
@@ -43,9 +59,41 @@ fun Movies(moviesViewModel: MoviesViewModel = hiltViewModel()) {
                     items(movies.value) { item ->
                         MovieItem(movie = item)
                     }
+
+                    item {
+                        CircularLoadingIndicator()
+                    }
+
                 }
             }
         }
 
+    }
+
+    LaunchedEffect(lazyListState) {
+        snapshotFlow { lazyListState.layoutInfo }
+            .collect { layoutInfo ->
+                val lastVisibleItemIndex = layoutInfo.visibleItemsInfo.lastOrNull()?.index
+                if (lastVisibleItemIndex == layoutInfo.totalItemsCount - 1) {
+                    launch {
+                        if (movies.value.isNotEmpty()) {
+                            moviesViewModel.incrementPage()
+                            moviesViewModel.getMovies()
+                        }
+                    }
+                }
+            }
+    }
+}
+
+@Composable
+fun CircularLoadingIndicator() {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(30.dp),
+        contentAlignment = Alignment.Center
+    ) {
+        CircularProgressIndicator()
     }
 }
--- app/src/main/java/com/tor/movies/ui/screen/movies/MoviesViewModel.kt
+++ app/src/main/java/com/tor/movies/ui/screen/movies/MoviesViewModel.kt
@@ -1,5 +1,6 @@
 package com.tor.movies.ui.screen.movies
 
+import androidx.compose.runtime.collectAsState
 import com.tor.common.base.BaseViewModel
 import com.tor.movies.repository.domain.usecase.GetMoviesUseCase
 import com.tor.movies.repository.model.Movie
@@ -21,44 +22,54 @@ class MoviesViewModel @Inject constructor(
 ) : BaseViewModel() {
 
     init {
-        getDataPeople()
+        getMovies()
     }
 
     private var _page = 1
     val page: Int get() = _page
 
+
+    private val _isShimmerShow = MutableStateFlow(true)
+    val isShimmerShow: Flow<Boolean> get() = _isShimmerShow.asStateFlow()
+
     private val _isMoviesEmpty = MutableStateFlow(false)
     val isMoviesEmpty: Flow<Boolean> get() = _isMoviesEmpty.asStateFlow()
-
 
     private val _movies = MutableStateFlow<List<Movie>>(emptyList())
     val movies: Flow<List<Movie>> get() = _movies.asStateFlow()
 
-    fun getDataPeople(title: String = DEFAULT_TITLE) {
-        setIsLoading(true)
+    fun getMovies(title: String = DEFAULT_TITLE) {
         coroutineScope.launch {
             try {
-                 getMoviesUseCase.invoke(title, _page).collect{ result ->
+                if (_movies.value.isEmpty()) {
+                    _isShimmerShow.emit(true)
+                }
+
+                getMoviesUseCase.invoke(title, _page).collect{ result ->
                      when (result) {
                          is Result.Success -> {
-                             _movies.emit(result.data.search)
-                             _isMoviesEmpty.emit(result.data.search.isEmpty())
+                             val newData = _movies.value + result.data.search
+                             _movies.emit(newData)
+                             _isMoviesEmpty.emit(newData.isEmpty())
                          }
                          is Result.Error -> {
                              _isMoviesEmpty.emit(true)
                          }
                      }
                  }
+
             } catch (e: Exception) {
-                _isMoviesEmpty.emit(true)
+                _isMoviesEmpty.emit(_movies.value.isEmpty())
             } finally {
-                setIsLoading(false)
+                _isShimmerShow.emit(false)
             }
         }
     }
 
-    fun setPage(page: Int) {
-        _page = page
+
+
+    fun incrementPage() {
+        _page = page + 1
     }
 
     companion object {
